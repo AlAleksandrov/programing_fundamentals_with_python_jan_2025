bb92e2a09bb4fdbec8c64665e0f11410af27fadd
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-02-01 13:21:40 +0200

    Create 06_calculate_rectangle_area.py

---
+++ 04_functions/01_lab/06_calculate_rectangle_area.py
@@ -0,0 +1,9 @@
+def area_of_rectangle(width, height):
+    result = 0
+    result = width * height
+    return result
+
+a = int(input())
+b = int(input())
+current_result = area_of_rectangle(a, b)
+print(current_result)
