136a371b31ca13f65a6bf0176b0548070dd45592
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-02-09 18:36:48 +0200

    Create 08_decipher_this!.py

---
+++ 05_lists_advanced/02_exercise/08_decipher_this!.py
@@ -0,0 +1,14 @@
+secret_message = input().split()
+ascii_value = 0
+string = ""
+for string in secret_message:
+    ascii_value_string = ""
+    for char in string:
+        if char.isdigit():
+            ascii_value_string = ascii_value_string + char
+    ascii_value = int(ascii_value_string)
+    if len(string) - len(ascii_value_string) == 1:
+        string = chr(ascii_value) + string[len(string) - 1:len(string):]
+    else:
+        string = chr(ascii_value) + string[len(string) - 1:len(string):] + string[len(ascii_value_string) + 1:len(string) - 1:] + string[len(ascii_value_string):len(ascii_value_string) + 1:]
+    print(string, end=" ")
