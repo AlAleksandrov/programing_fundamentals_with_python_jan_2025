05e8819ece95359a34c4c6cf9d5ff475b92a10ac
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-03-01 15:58:52 +0200

    Create 02_stock.py

---
+++ 09_dictionaries/01_lab/02_stock.py
@@ -0,0 +1,15 @@
+elements = input().split()
+stock = input().split()
+bakery = {}
+for i in range(0, len(elements), 2):
+    product = elements[i]
+    quantity = elements[i + 1]
+    bakery[product] = quantity
+
+for i in range(0, len(stock)):
+    product = stock[i]
+    if stock[i] in elements:
+        quantity = int(bakery.get(product))
+        print(f"We have {quantity} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
