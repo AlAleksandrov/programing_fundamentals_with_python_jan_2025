6394d60587529c341772bad93a075dd3d10fe93d
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-03-01 16:43:26 +0200

    Create 03_statistics.py

---
+++ 09_dictionaries/01_lab/03_statistics.py
@@ -0,0 +1,23 @@
+count_all_products = 0
+sum_all_quantities = 0
+bakery = {}
+
+while True:
+    command = input().split(": ")
+
+    if command[0] == "statistics":
+        print("Products in stock:")
+        for product, quantity in bakery.items():
+            print(f"- {product}: {quantity}")
+        print(f"Total Products: {count_all_products}")
+        print(f"Total Quantity: {sum_all_quantities}")
+        break
+    product = command[0]
+    quantity = int(command[1])
+    sum_all_quantities += quantity
+    if product not in bakery:
+        bakery[product] = quantity
+        count_all_products += 1
+    else:
+        quantity += int(bakery[product])
+        bakery[product] = quantity
