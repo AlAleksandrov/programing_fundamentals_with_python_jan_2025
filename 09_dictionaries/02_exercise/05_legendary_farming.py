8787c59a2dbbfe1abad7353233d327ee61c4dddc
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-03-02 18:09:20 +0200

    Update 05_legendary_farming.py

--- 09_dictionaries/02_exercise/05_legendary_farming.py
+++ 09_dictionaries/02_exercise/05_legendary_farming.py
@@ -16,17 +16,17 @@ while not obtained_legendary_item:
         if materials["shards"] >= 250:
             legendary_item = "Shadowmourne"
             obtained_legendary_item = True
-            materials[material] -= 250
+            materials["shards"] -= 250
 
         elif materials["fragments"] >= 250:
             legendary_item = "Valanyr"
             obtained_legendary_item = True
-            materials[material] -= 250
+            materials["fragments"] -= 250
 
         elif materials["motes"] >= 250:
             legendary_item = "Dragonwrath"
             obtained_legendary_item = True
-            materials[material] -= 250
+            materials["motes"] -= 250
 
         if obtained_legendary_item:
             print(f"{legendary_item} obtained!")
