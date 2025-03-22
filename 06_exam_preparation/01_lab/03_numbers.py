e9d3bc8b7605b0b03fdf7b414d8dc5d8d1f8a47a
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-03-22 14:04:29 +0200

    Delete 03_numbers.py

--- 06_exam_preparation/01_lab/03_numbers.py
+++
@@ -1,9 +0,0 @@
-sequence_of_integers = list(map(int, input().split()))
-average = sum(sequence_of_integers) / len(sequence_of_integers)
-numbers_greater_average = [number for number in sequence_of_integers if number > average]
-
-if numbers_greater_average:
-    five_nums = sorted(numbers_greater_average, reverse=True)[:5]
-    print(" ".join(map(str, five_nums)))
-else:
-    print("No")
